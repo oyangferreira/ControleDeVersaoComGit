779b02dda19c072947716e930f7470c8684a0cdb
oyangferreira <yang.ferreira2023@gmail.com>
2024-07-03 05:32:32 -0300

    Mudança de texto

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Teste em C#");
 Console.WriteLine(DateTime.Now.ToLongDateString());
 Console.WriteLine("Versão da aplicação: 1.0");
